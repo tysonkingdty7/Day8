8b7195b433fb944e98cbf7c2fd70e847cec14a4c
tyson <tysonosman@gmail.com>
2023-09-28 14:38:10 +0300

    day8 csharp

---
+++ Day8/Day8/Program.cs
@@ -0,0 +1,64 @@
+﻿
+
+using System.Collections.Generic;
+
+namespace Day8
+{
+    public class Program
+    {
+
+        public static void Main()
+
+        {
+
+            List<int> myList = new List<int> { 5, 2, 7, 1, 3 };
+            Console.WriteLine("1- show first number");
+            Console.WriteLine("2-show last number");
+            Console.WriteLine("3- number %2");
+            Console.WriteLine("4-Show first number and scend number");
+            Console.WriteLine("5-show number *number");
+            Console.WriteLine("6-Exit");
+            int choice = int.Parse(Console.ReadLine());
+            Console.WriteLine("----------------------------------------");
+            switch (choice)
+            {
+                case 1:
+                    int first = myList.First();
+                    Console.WriteLine(first);
+                    break;
+                        case 2:
+                    int last = myList.Last();
+                    Console.WriteLine(last);
+                    break;
+                        case 3:
+                    IEnumerable<int> evenNumbers = myList.Where(x => x % 2 == 0);
+                    Console.WriteLine(string.Join(", ", evenNumbers));
+                    break;
+                    case 4:
+                    IEnumerable<int> firstTwo = myList.Take(2);
+                    Console.WriteLine(string.Join(", ", firstTwo));
+                    break;
+                case 5:
+                    IEnumerable<int> squaredList = myList.Select(x => x * x);
+                    Console.WriteLine(string.Join(", ", squaredList));
+                    break;
+
+
+            }
+
+
+
+
+            //    int last = myList.Last();
+            //    Console.WriteLine(last);   
+
+           
+
+        
+
+           
+            //
+        }
+
+    }
+}
---
+++ Day8/Day8/own.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class MyExtensions
+{
+    public static T First<T>(this IEnumerable<T> source)
+    {
+        foreach (var item in source)
+        {
+            return item;
+        }
+
+        throw new InvalidOperationException("The source sequence is empty.");
+    }
+
+    public static T Last<T>(this IEnumerable<T> source)
+    {
+        T lastItem = default(T);
+        bool found = false;
+
+        foreach (var item in source)
+        {
+            lastItem = item;
+            found = true;
+        }
+
+        if (!found)
+        {
+            throw new InvalidOperationException("The source sequence is empty.");
+        }
+
+        return lastItem;
+    }
+
+    public static IEnumerable<T> Where<T>(this IEnumerable<T> source, Func<T, bool> predicate)
+    {
+        foreach (var item in source)
+        {
+            if (predicate(item))
+            {
+                yield return item;
+            }
+        }
+    }
+
+    public static IEnumerable<T> Take<T>(this IEnumerable<T> source, int count)
+    {
+        int taken = 0;
+
+        foreach (var item in source)
+        {
+            if (taken >= count)
+            {
+                yield break;
+            }
+
+            yield return item;
+            taken++;
+        }
+    }
+
+    public static IEnumerable<T> OrderBy<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector)
+    {
+        return source.OrderBy(keySelector);
+    }
+
+    //public static IEnumerable<T> OrderByDescending<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector)
+    //{
+    //    return source.OrderByDescending(keySelector);
+    //}
+
+    public static IEnumerable<TResult> Select<T, TResult>(this IEnumerable<T> source, Func<T, TResult> selector)
+    {
+        foreach (var item in source)
+        {
+            yield return selector(item);
+        }
+    }
+}
